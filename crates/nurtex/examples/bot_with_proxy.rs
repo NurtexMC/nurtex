5eb71830ba6ec5f0e26ae7865e6cfd0c26640025
nullclyze <fliget45d1mw@gmail.com>
2026-05-01 01:33:09 +0400

    An example of creating a bot with a proxy added

---
+++ crates/nurtex/examples/bot_with_proxy.rs
@@ -0,0 +1,29 @@
+use std::time::Duration;
+
+use nurtex::bot::{Bot, BotChatExt};
+use nurtex::proxy::Proxy;
+
+/// Адрес SOCKS5 прокси, например `164.85.71.8:2749`
+const PROXY_ADDRESS: &str = "YOUR_SOCKS5_PROXY";
+
+/// Хост **публичного** сервера, например `mc.server.com` 
+const SERVER_HOST: &str = "PUBLIC_HOST";
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+  // Создаём бота с прокси
+  let proxy = Proxy::new(PROXY_ADDRESS);
+  let mut bot = Bot::create_with_proxy("nurtex_bot", proxy);
+
+  // Подключаем бота к публичному серверу
+  bot.connect(SERVER_HOST, 25565);
+
+  // Ждём немножко
+  tokio::time::sleep(Duration::from_secs(8)).await;
+
+  // Отправляем сообщение в чат
+  bot.chat_message(format!("Привет, я {}!", bot.username())).await?;
+
+  // Ожидаем окончания хэндла подключения
+  bot.wait_handle().await
+}
